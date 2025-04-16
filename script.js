869f266b8128dc1097ad1e7e925dae0c3a4b14bf
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-04-16 11:33:48 +0600

    Updated

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const productList = document.querySelector('.products')
 
 productList.addEventListener('click', (e) => {
     if(e.target.tagName === "SPAN") {
-        window.location.href += "/" + e.target.className
+        window.location.href += e.target.className
     } else if(e.target.closest("SPAN")) {
-        window.location.href += "/" + e.target.closest("SPAN").className
+        window.location.href += e.target.closest("SPAN").className
     }
 })
